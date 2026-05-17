59f38a8eb315f241b67d489f2a1beb3456533634
Prarambha Barman <prarambhabarman108@gmail.com>
2026-05-17 17:02:42 +0530

    Create gas_constant.py

---
+++ basics/gas_constant.py
@@ -0,0 +1,6 @@
+ # Avogadro constant (mol-1): exact value by definition.
+N_A = 602_214_076_000_000_000_000_000
+ # Boltzmann constant (J.K-1): exact value by definition.
+k_B = 1.380649e-23
+R=N_A*k_B # the gas constant (J.K-1.mol-1)
+print(R)
